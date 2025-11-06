4ebbfc51405d7f0b16e48ecae427278fa7feb162
vane553 <vanelizagui1@gmail.com>
2025-11-05 19:09:13 -0600

    main

--- src/main/java/com/pixelcode/materiascrud/service/MateriaService.java
+++ src/main/java/com/pixelcode/materiascrud/service/MateriaService.java
@@ -1,6 +1,7 @@
 package com.pixelcode.materiascrud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,4 +19,23 @@ public class MateriaService {
         return materiaRepository.findAll();
     }
 
+    public Optional<MateriaEntity> getMateriaById(Integer id) {
+        return materiaRepository.findById(id);
+    }
+
+    public MateriaEntity createMateria(MateriaEntity materia) {
+        return materiaRepository.save(materia);
+    }
+
+    public MateriaEntity updateMateria(Integer id, MateriaEntity materiaDetails) {
+        MateriaEntity materia = materiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Materia no encontrada con id: " + id));
+
+        materia.setNombre(materiaDetails.getNombre());
+        materia.setCodigo(materiaDetails.getCodigo());
+        materia.setCuatrimestre(materiaDetails.getCuatrimestre());
+        materia.setActivo(materiaDetails.isActivo());
+
+        return materiaRepository.save(materia);
+    }
 }
