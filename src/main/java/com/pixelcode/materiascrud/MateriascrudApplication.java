33ae223d5b2cc386a4d30662d885b1e25296aad1
vane553 <vanelizagui1@gmail.com>
2025-11-05 18:28:13 -0600

    primer commit

---
+++ src/main/java/com/pixelcode/materiascrud/MateriascrudApplication.java
@@ -0,0 +1,13 @@
+package com.pixelcode.materiascrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateriascrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateriascrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixelcode/materiascrud/MateriascrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelcode.materiascrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateriascrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
