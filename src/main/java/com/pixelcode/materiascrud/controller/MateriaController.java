29eb29d26d72716fbdf2ca1f27ae98015a3a42cc
vane553 <vanelizagui1@gmail.com>
2025-11-05 18:51:29 -0600

    main

--- src/main/java/com/pixelcode/materiascrud/controller/MateriaController.java
+++ src/main/java/com/pixelcode/materiascrud/controller/MateriaController.java
@@ -1,5 +1,28 @@
 package com.pixelcode.materiascrud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pixelcode.materiascrud.entity.MateriaEntity;
+import com.pixelcode.materiascrud.service.MateriaService;
+
+@RestController
+@RequestMapping("/api/materias")
 public class MateriaController {
+
+     @Autowired
+    private MateriaService materiaService;
+
+    // Obtener todas las materias activas
+    @GetMapping
+    public ResponseEntity<List<MateriaEntity>> getAllMaterias() {
+        List<MateriaEntity> materias = materiaService.getAllMaterias();
+        return ResponseEntity.ok(materias);
+    }
 
 }
--- src/main/java/com/pixelcode/materiascrud/service/MateriaService.java
+++ src/main/java/com/pixelcode/materiascrud/service/MateriaService.java
@@ -1,5 +1,21 @@
 package com.pixelcode.materiascrud.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pixelcode.materiascrud.entity.MateriaEntity;
+import com.pixelcode.materiascrud.repository.MateriaRepository;
+
+@Service
 public class MateriaService {
+
+    @Autowired
+    private MateriaRepository materiaRepository;
+
+    public List<MateriaEntity> getAllMaterias() {
+        return materiaRepository.findAll();
+    }
 
 }
