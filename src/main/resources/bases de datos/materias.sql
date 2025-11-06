bdd78e2d9c629a30bfe40d02cad41c8907e1e053
vane553 <vanelizagui1@gmail.com>
2025-11-05 19:47:28 -0600

    main

---
+++ src/main/resources/bases de datos/materias.sql
@@ -0,0 +1,6 @@
+INSERT INTO materias (nombre, codigo, activo, cuatrimestre) VALUES
+('Programación Orientada a Objetos', 'POO101', true, 'Segundo'),
+('Bases de Datos', 'BD202', true, 'Tercero'),
+('Desarrollo Web', 'DW303', true, 'Cuarto'),
+('Ingeniería de Software', 'IS404', true, 'Quinto'),
+('Sistemas Operativos', 'SO505', true, 'Sexto');
